c6821849d6d88d504e3b4b1a5be7b8426048631a
Sagor Saha <sagorsahadev00@gmail.com>
2026-09-15 23:24:56 +0600

    gg

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Express } from "express";
 import cors from "cors";
 import buyerRouter from "./router/buyerRouter.js";
 import OrderRouter from "./router/orderRouter.js";
@@ -7,31 +7,29 @@ import productRouter from "./router/productRouter.js";
 
 import "dotenv/config";
 
-const app = express();
+const app: Express = express();
+app.use(express.json());
 
 app.use(
   cors({
-    origin: "*", // Vercel deployment-এর জন্য আপাতত '*' রাখুন
+    origin: "http://localhost:3000",
   }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("server is ok no tension");
 });
 
-// Routers
+//router
 app.use("/api/buyer", buyerRouter);
 app.use("/api/order", OrderRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
+const PORT = 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 export default app;
