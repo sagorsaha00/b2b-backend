60568f8d6eadd67014de84e14306a11df3d7b428
Sagor Saha <sagorsahadev00@gmail.com>
2026-09-15 23:39:59 +0600

    c

--- database/index.ts
+++ database/index.ts
@@ -1,17 +1,14 @@
 import "dotenv/config";
-
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma_client/client.js";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DIRECT_URL;
+
 if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
-const adapter = new PrismaPg({
-  connectionString,
-});
-const prisma = new PrismaClient({
-  adapter,
-});
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
